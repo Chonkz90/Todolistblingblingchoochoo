e745493d742e4a9b2bdcb9439c2077092b4bab74
Chonkz90 <zahlerdal@yahoo.com>
2024-09-12 08:51:41 +0200

    nesten

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function newElement() {
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
-      alert("Du må skrive noe.");
+      alert("Du må skrive noe..");
     } else {
       document.getElementById("myUL").appendChild(li);
     }
