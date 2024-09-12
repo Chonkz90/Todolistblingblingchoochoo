5d4c73a943f7dc1dbeca4f02158661b5bd4399d7
Chonkz90 <zahlerdal@yahoo.com>
2024-09-12 08:47:53 +0200

    90%

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function newElement() {
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
-      alert("You must write something!");
+      alert("Du må skrive noe.");
     } else {
       document.getElementById("myUL").appendChild(li);
     }
